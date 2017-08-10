8efae9023e16236eff373deafd44312197f8b03d
maximilianomohamed <mohamedmaximiliano@gmail.com>
2017-08-10 20:56:39 -0300

    modificación vista articulos lutheria

--- controller/lutheria.php
+++ controller/lutheria.php
@@ -2,18 +2,32 @@
 
 	session_start();
 	require_once('./configTwig.php');
-	
-		$datos['lutheria'] = true;
-		if(isset($_GET['maderas'])){
-			renderizar('accesorios.html',$datos);
-		}
-		if(isset($_GET['herramientas'])){
-			renderizar('accesorios.html',$datos);
-		}
-		if(isset($_GET['accesoriosLutheria'])){
-			renderizar('accesorios.html',$datos);
-		}
+	require_once('../model/abmUsados.php');
+	require_once('../model/abmProductoNuevo.php');
+	require_once('../model/abmCategoria.php');
 
+	$datos['categorias'] = detalleCategoria();
+	/*	if(isset($_GET['maderas'])){
+				renderizar('accesorios.html',$datos);
+			}
+			if(isset($_GET['herramientas'])){
+				renderizar('accesorios.html',$datos);
+			}
+			if(isset($_GET['accesoriosLutheria'])){
+				renderizar('accesorios.html',$datos);
+		}*/
+		if(isset($_GET['discos'])){
+			$datos['productosNuevos']=listaProductosNuevos('Maderas');
+			renderizar('accesorios.html',$datos);
+		}
+		if(isset($_GET['instrumentos'])){
+			$datos['productosNuevos']=listaProductosNuevos('Herramientas');
+			renderizar('accesorios.html',$datos);
+		}
+		if(isset($_GET['libros'])){
+			$datos['productosNuevos']=listaProductosNuevos('Accesorios lutheria');
+			renderizar('accesorios.html',$datos);
+		}
 
 
 ?>
--- controller/productos.php
+++ controller/productos.php
@@ -9,27 +9,48 @@
 		$datos['categorias'] = detalleCategoria();
 		if(isset($_GET['accesorios'])){
 			$datos['productosNuevos']=listaProductosNuevos('Accesorios');
+			$datos['tipo'] = 'Accesorios';
 			renderizar('accesorios.html',$datos);
 		}
 		if(isset($_GET['amplificadores'])){
 			$datos['productosNuevos']=listaProductosNuevos('Amplificadores');
+			$datos['tipo'] = 'Amplificadores';
 			renderizar('accesorios.html',$datos);
 		}
 		if(isset($_GET['discos'])){
 			$datos['productosNuevos']=listaProductosNuevos('Discos');
+			$datos['tipo'] = 'Discos';
 			renderizar('accesorios.html',$datos);
 		}
 		if(isset($_GET['instrumentos'])){
 			$datos['productosNuevos']=listaProductosNuevos('Instrumentos');
+			$datos['tipo'] = 'Instrumentos';
 			renderizar('accesorios.html',$datos);
 		}
 		if(isset($_GET['libros'])){
 			$datos['productosNuevos']=listaProductosNuevos('Libros');
+			$datos['tipo'] = 'Libros';
 			renderizar('accesorios.html',$datos);
 		}
 		if(isset($_GET['usados'])){
 			$datos['categoria']='usados';
 			$datos['usados']=listaUsados('usado');
+			$datos['tipo'] = 'Usados';
+			renderizar('accesorios.html',$datos);
+		}
+		if(isset($_GET['maderas'])){
+			$datos['productosNuevos']=listaProductosNuevos('Maderas');
+			$datos['tipo'] = 'Maderas';
+			renderizar('accesorios.html',$datos);
+		}
+		if(isset($_GET['herramientas'])){
+			$datos['productosNuevos']=listaProductosNuevos('Herramientas');
+			$datos['tipo'] = 'Herramientas';
+			renderizar('accesorios.html',$datos);
+		}
+		if(isset($_GET['accesorioslutheria'])){
+			$datos['productosNuevos']=listaProductosNuevos('Accesorios lutheria');
+			$datos['tipo'] = 'Accesorios Luthería';
 			renderizar('accesorios.html',$datos);
 		}
 		
